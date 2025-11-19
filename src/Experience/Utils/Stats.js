fee0c15f12115c884e3b2e7180d651f22ae05a88
Jérémy <jeremy.brialon@pm.me>
2025-11-19 14:04:59 +0100

    🐛 update debug / fix loader

--- src/Experience/Utils/Stats.js
+++ src/Experience/Utils/Stats.js
@@ -2,23 +2,33 @@ import Stats from "stats-gl";
 
 export default class Performance {
   constructor() {
-    this.active = window.location.hash === "#debug";
+    this.active = false;
 
-    if (this.active) {
-      this.stats = new Stats({
-        trackGPU: true,
-        trackHz: true,
-        trackCPT: true,
-        minimal: false,
-        mode: 2,
-        horizontal: false,
-      });
-      document.body.appendChild(this.stats.dom);
-    }
+    this.stats = new Stats({
+      trackGPU: true,
+      trackHz: true,
+      trackCPT: true,
+      minimal: false,
+      mode: 2,
+      horizontal: false,
+    });
+    document.body.appendChild(this.stats.dom);
+    this.stats.dom.style.display = this.active ? "block" : "none";
+
+    this.setupKeyboardToggle();
+  }
+
+  setupKeyboardToggle() {
+    window.addEventListener("keydown", (event) => {
+      if (event.key === "d" || event.key === "D") {
+        this.active = !this.active;
+        this.stats.dom.style.display = this.active ? "block" : "none";
+      }
+    });
   }
 
   init(renderer) {
-    if (this.active) this.stats.init(renderer);
+    this.stats.init(renderer);
   }
 
   begin() {
